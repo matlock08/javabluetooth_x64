ddd2fd622236e8944195cc496249bdb511dd23da
unknown <jose.castellanos.molina@gmail.com>
2018-02-19 11:36:31 -0600

    Added parse message

--- src/main/java/com/roni/blue/ObexPutClient.java
+++ src/main/java/com/roni/blue/ObexPutClient.java
@@ -33,7 +33,7 @@ public class ObexPutClient {
         OutputStream out = clientSession.openOutputStream();
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
         int sum = 0;
-        byte[] sendBuffer = {'F','T',0,0,0x07,0,0,0,0}; 
+        byte[] sendBuffer = {'F','T',0,0,0x08,0,0,0,0}; 
         for (int i=0;i<7;i++) {
             sum += sendBuffer[i];
         }
@@ -47,16 +47,28 @@ public class ObexPutClient {
 
         InputStream in = clientSession.openInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        char[] buff = new char[32];
-        br.read(buff, 0, 32 );
+        char[] buff = new char[9];
+        char[] buffer = new char[512];
+        br.read(buff, 0, 9 );
         System.out.println("RCV ");
-        for (int i = 0; i < 32; i++ ) {
-            System.out.print( String.format("%04x ", (int)buff[i]) );
-        }
-        for (int i = 0; i < 32; i++ ) {
-            System.out.print( buff[i] );
-        }
-        System.out.println();
+        
+        System.out.print( buff[0] ); // F
+        System.out.print( buff[1] ); // T
+        System.out.print( " " );
+        System.out.print( String.format("%04x ", (int)buff[4]) ); // Command 0x08
+        int size=(byte)(buff[5])+((buff[6]<<8)&0xFF00)-1;
+        System.out.print( " " );
+        System.out.print( size ); // Size
+        System.out.print( " " );
+        System.out.print( (int)buff[7] ); // Status 
+        br.read(buffer, 0, size );
+
+	byte buf[]=new byte[1024];
+        // memcpy(buf,0,buffer,0,512); // From DB
+        // memcpy(buf,512,buffer,0,512); // From Reader
+
+        
+        
         
         
         clientSession.close();
