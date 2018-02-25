8d91944eca4aee5f532126a1ce8a5c7e61145d72
Jose Castellanos <jose.castellanos.molina@gmail.com>
2018-02-25 17:44:23 -0600

    Refactored

--- src/main/java/com/roni/blue/RemoteDeviceDiscovery.java
+++ src/main/java/com/roni/blue/RemoteDeviceDiscovery.java
@@ -1,55 +1,60 @@
 package rom.roni.blue;
 
 import java.io.IOException;
-import java.util.Vector;
+import java.util.List;
+import java.util.ArrayList;
 import javax.bluetooth.*;
 
 /**
  * Minimal Device Discovery example.
  */
-public class RemoteDeviceDiscovery {
+public class RemoteDeviceDiscovery implements DiscoveryListener {
 
-    public static final Vector/*<RemoteDevice>*/ devicesDiscovered = new Vector();
+    private final List<RemoteDevice> devicesDiscovered = new ArrayList<RemoteDevice>();
+    final Object inquiryCompletedEvent = new Object();
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-
-        final Object inquiryCompletedEvent = new Object();
-
+    public RemoteDeviceDiscovery() throws BluetoothStateException, InterruptedException {
         devicesDiscovered.clear();
 
-        DiscoveryListener listener = new DiscoveryListener() {
-
-            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
-                System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
-                devicesDiscovered.addElement(btDevice);
-                try {
-                    System.out.println("     name " + btDevice.getFriendlyName(false));
-                } catch (IOException cantGetDeviceName) {
-                }
-            }
-
-            public void inquiryCompleted(int discType) {
-                System.out.println("Device Inquiry completed!");
-                synchronized(inquiryCompletedEvent){
-                    inquiryCompletedEvent.notifyAll();
-                }
-            }
-
-            public void serviceSearchCompleted(int transID, int respCode) {
-            }
-
-            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
-            }
-        };
-
         synchronized(inquiryCompletedEvent) {
-            boolean started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
+            boolean started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, this);
             if (started) {
                 System.out.println("wait for device inquiry to complete...");
                 inquiryCompletedEvent.wait();
                 System.out.println(devicesDiscovered.size() +  " device(s) found");
             }
         }
+    }
+
+    public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+        System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
+        devicesDiscovered.add(btDevice);
+        try {
+            System.out.println("     name " + btDevice.getFriendlyName(false));
+        } catch (IOException cantGetDeviceName) {
+        }
+    }
+
+    public void inquiryCompleted(int discType) {
+        System.out.println("Device Inquiry completed!");
+        synchronized(inquiryCompletedEvent){
+            inquiryCompletedEvent.notifyAll();
+        }
+    }
+
+    public void serviceSearchCompleted(int transID, int respCode) {
+    }
+
+    public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+    }
+
+    public List<RemoteDevice> getRemoteDevices() {
+        return devicesDiscovered;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new RemoteDeviceDiscovery();
+        
     }
 
 }
--- src/main/java/com/roni/blue/ServicesSearch.java
+++ src/main/java/com/roni/blue/ServicesSearch.java
@@ -2,26 +2,24 @@ package rom.roni.blue;
 
 import java.io.IOException;
 import java.util.Enumeration;
-import java.util.Vector;
+import java.util.List;
+import java.util.ArrayList;
 import javax.bluetooth.*;
 
 /**
  *
  * Minimal Services Search example.
  */
-public class ServicesSearch {
+public class ServicesSearch implements DiscoveryListener {
 
     static final UUID OBEX_OBJECT_PUSH = new UUID(0x0003);
-
     static final UUID SPP = new UUID("446118f08b1e11e29e960800200c9a66", false);
+    private final List<String> serviceFound = new ArrayList<String>();
+    private final Object serviceSearchCompletedEvent = new Object();
 
-
-    public static final Vector/*<String>*/ serviceFound = new Vector();
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-
+    public ServicesSearch(String args[]) throws IOException,InterruptedException  {
         // First run RemoteDeviceDiscovery and use discoved device
-        RemoteDeviceDiscovery.main(null);
+        RemoteDeviceDiscovery discovery = new RemoteDeviceDiscovery();
         
         serviceFound.clear();
 
@@ -30,55 +28,57 @@ public class ServicesSearch {
             serviceUUID = new UUID(args[0], false);
         }
 
-        final Object serviceSearchCompletedEvent = new Object();
-
-        DiscoveryListener listener = new DiscoveryListener() {
-
-            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
-            }
-
-            public void inquiryCompleted(int discType) {
-            }
-
-            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
-                for (int i = 0; i < servRecord.length; i++) {
-                    String url = servRecord[i].getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
-                    if (url == null) {
-                        continue;
-                    }
-                    serviceFound.add(url);
-                    DataElement serviceName = servRecord[i].getAttributeValue(0x0100);
-                    if (serviceName != null) {
-                        System.out.println("service " + serviceName.getValue() + " found " + url);
-                    } else {
-                        System.out.println("service found " + url);
-                    }
-                }
-            }
-
-            public void serviceSearchCompleted(int transID, int respCode) {
-                System.out.println("service search completed!");
-                synchronized(serviceSearchCompletedEvent){
-                    serviceSearchCompletedEvent.notifyAll();
-                }
-            }
-
-        };
-
         UUID[] searchUuidSet = new UUID[] { serviceUUID };
         int[] attrIDs =  new int[] {
                 0x0100 // Service name
         };
 
-        for(Enumeration en = RemoteDeviceDiscovery.devicesDiscovered.elements(); en.hasMoreElements(); ) {
-            RemoteDevice btDevice = (RemoteDevice)en.nextElement();
-
+        for(RemoteDevice btDevice : discovery.getRemoteDevices() ) {
+            
             synchronized(serviceSearchCompletedEvent) {
                 System.out.println("search services on " + btDevice.getBluetoothAddress() + " " + btDevice.getFriendlyName(false));
-                LocalDevice.getLocalDevice().getDiscoveryAgent().searchServices(attrIDs, searchUuidSet, btDevice, listener);
+                LocalDevice.getLocalDevice().getDiscoveryAgent().searchServices(attrIDs, searchUuidSet, btDevice, this);
                 serviceSearchCompletedEvent.wait();
             }
         }
+    }
+
+    public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+    }
+
+    public void inquiryCompleted(int discType) {
+    }
+
+    public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+        for (int i = 0; i < servRecord.length; i++) {
+            String url = servRecord[i].getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
+            if (url == null) {
+                continue;
+            }
+            serviceFound.add(url);
+            DataElement serviceName = servRecord[i].getAttributeValue(0x0100);
+            if (serviceName != null) {
+                System.out.println("service " + serviceName.getValue() + " found " + url);
+            } else {
+                System.out.println("service found " + url);
+            }
+        }
+    }
+
+    public void serviceSearchCompleted(int transID, int respCode) {
+        System.out.println("service search completed!");
+        synchronized(serviceSearchCompletedEvent){
+            serviceSearchCompletedEvent.notifyAll();
+        }
+    }
+
+    public List<String> getServiceFound() {
+        return serviceFound;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        new ServicesSearch(args);
 
     }
 
